7a0df5a600cc967a78def569d6ae3169c3447f89
bharat <112468018+One-hope-bharat@users.noreply.github.com>
2024-05-17 15:01:05 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-
 PROD_REPO="1hopebharat/capstone_prod"
+EC2_HOST="54.218.80.37"
+SSH_KEY="/home/ec2-user/keypair/capstone_web_oregon.pem"
+SSH_USER="ec2-user"
 
 if [[ -f "version.txt" ]]; then
     VERSION_TAG=$(cat version.txt)
@@ -10,10 +12,13 @@ else
     echo "Error: version.txt not found"
     exit 1
 fi
+ssh -i "$SSH_KEY" "$SSH_USER@$EC2_HOST" "$SSH_COMMAND"
 
-docker pull $PROD_REPO:$VERSION_TAG
+SSH_COMMAND="docker pull $PROD_REPO:$VERSION_TAG && \
+             docker stop capstone_container || true && \
+             docker rm capstone_container || true && \
+             docker run -d --name capstone_container -p 80:80 $PROD_REPO:$VERSION_TAG"
 
-docker stop capstone_container || true && docker rm capstone_container || true
 
-docker run -itd --name capstone_container -p 80:80 $PROD_REPO:$VERSION_TAG
+
 
