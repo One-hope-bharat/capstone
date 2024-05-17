4bc391d1bbb2fa0a7299194d3279920e16167a06
bharat <112468018+One-hope-bharat@users.noreply.github.com>
2024-05-17 16:47:59 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-DEV_REPO="1hopebharat/capstone_dev"
+PROD_REPO="1hopebharat/capstone_prod"
 if [[ -f "version.txt" ]]; then
     VERSION_TAG=$(cat version.txt)
 else
     echo "Error: version.txt not found"
     exit 1
 fi
-docker pull $DEV_REPO:$VERSION_TAG
+docker pull $PROD_REPO:$VERSION_TAG
 docker stop capstone_container || true && docker rm capstone_container || true
-docker run -itd --name capstone_container -p 80:80 $DEV_REPO:$VERSION_TAG
+docker run -itd --name capstone_container -p 80:80 $PROD_REPO:$VERSION_TAG
