94cd8a5687543d6bc54972ed2c225eaecb55044d
bharat <112468018+One-hope-bharat@users.noreply.github.com>
2024-05-18 17:39:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-# Define Docker Hub repository URLs
 PROD_REPO="1hopebharat/capstone_prod"
 DEV_REPO="1hopebharat/capstone_dev"
 
-# Retrieve the version tag from the file created by build.sh
 if [[ -f "version.txt" ]]; then
     VERSION_TAG=$(cat version.txt)
 else
@@ -14,27 +12,14 @@ else
     exit 1
 fi
 
-
-
 REPO_URL=$DEV_REPO
 
-
-# Tag the Docker image with repository URL and version tag
 docker tag image_capstone:$VERSION_TAG $DEV_REPO:$VERSION_TAG
 
-# Authenticate Docker Hub login (ensure Docker Hub credentials are set as secrets)
 if [[ -z "$DOCKERHUB_CREDENTIALS_USR" || -z "$DOCKERHUB_CREDENTIALS_PSW" ]]; then
     echo "Error: Docker Hub credentials are not provided"
     exit 1
 fi
-
 echo "$DOCKERHUB_CREDENTIALS_PSW" | docker login -u "$DOCKERHUB_CREDENTIALS_USR" --password-stdin
-
-# Push the tagged Docker image to the Docker Hub repository
 docker push $DEV_REPO:$VERSION_TAG
-
-
-
-
-######
 
