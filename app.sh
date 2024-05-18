d2bf5a705066ff500aee444666baf7458b4def21
bharat <112468018+One-hope-bharat@users.noreply.github.com>
2024-05-19 00:11:43 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -7,6 +7,7 @@ else
     echo "Error: version.txt not found"
     exit 1
 fi
+echo "${DOCKERHUB_CREDENTIALS_PSW}" | docker login -u "${DOCKERHUB_CREDENTIALS_USR}" --password-stdin
 docker pull $PROD_REPO:$VERSION_TAG
 docker stop capstone_container || true && docker rm capstone_container || true
 docker run -itd --name capstone_container -p 80:80 $PROD_REPO:$VERSION_TAG
